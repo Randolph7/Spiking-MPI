74030a82e0e9e85a44f4d3e7a10d14df02ba13f6
Randolph <lizecheng@gdiist.cn>
2024-03-07 16:14:37 +0900

    First commit

---
+++ build/CMakeFiles/sMPI.dir/common.c.obj.d
@@ -0,0 +1,10 @@
+CMakeFiles/sMPI.dir/common.c.obj: \
+ /home/randolph/Develop/cmake_rv/sMPI/src/common.c \
+ /home/randolph/Develop/cmake_rv/lib/gcc/riscv32-unknown-elf/13.2.0/include/stdint.h \
+ /home/randolph/Develop/cmake_rv/riscv32-unknown-elf/include/stdint.h \
+ /home/randolph/Develop/cmake_rv/riscv32-unknown-elf/include/machine/_default_types.h \
+ /home/randolph/Develop/cmake_rv/riscv32-unknown-elf/include/sys/features.h \
+ /home/randolph/Develop/cmake_rv/riscv32-unknown-elf/include/_newlib_version.h \
+ /home/randolph/Develop/cmake_rv/riscv32-unknown-elf/include/sys/_intsup.h \
+ /home/randolph/Develop/cmake_rv/riscv32-unknown-elf/include/sys/_stdint.h \
+ /home/randolph/Develop/cmake_rv/sMPI/src/../include/common.h
