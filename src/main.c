cb22c2a55fcc9f83bceaa7936dbca3a0d2f4a7f3
Randolph <li_ze_cheng@yeah.net>
2024-03-18 19:01:25 +0900

    Fix map file problem.

--- src/main.c
+++ src/main.c
@@ -9,13 +9,15 @@
  */
 #include "pic.h"
 #include <sMPI.h>
-
+#include "common.h"
+#include <stdlib.h>
 int router_rcv_int_flag;//中断的flag
 
 //定义中断函数
 void router_intr()
 {	
 	router_rcv_int_flag = 1;
+  int i = rand();
 }
 
 int main(void)
@@ -23,13 +25,7 @@ int main(void)
 
     //init the router interrupt
 	enable_int(INT_ROUTER);
-    
-    // 定义并初始化一个整数数组
-    int array[] = {1, 2, 3, 4};
-    int *buf = array;
 
-    sMPI_router_trans_test(nonspike, sMPI_East, 1);
-    //sMPI_router_bcast_test(buf, sMPI_East, sMPI_South, sMPI_West, sMPI_North);
 
     while(1)
     {
@@ -37,8 +33,7 @@ int main(void)
 		{
 			router_rcv_int_flag = 0;
 			sMPI_router_recv_test();
-			char *intstr = "I am in the interrupt!\n";
-			print_str(intstr);
+      write_reg32(0x4000C000, 0xFF);//
 		}
     }
 
