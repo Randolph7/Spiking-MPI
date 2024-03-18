f50fa0b90d034888274ca6e79b585de200e212d6
Randolph <li_ze_cheng@yeah.net>
2024-03-18 13:30:01 +0900

    new bin

--- src/main.c
+++ src/main.c
@@ -462,11 +462,11 @@ int main(void)
 {
 
     //init the router interrupt
-	enable_int(INT_ROUTER);
+	  enable_int(INT_ROUTER);
     
     // 定义并初始化一个整数数组
-    int array[] = {1, 2, 3, 4};
-    int *buf = array;
+    //int array[] = {1, 2, 3, 4};
+    //int *buf = array;
 
     sMPI_router_trans_test(nonspike, sMPI_East, 1);
     //sMPI_router_bcast_test(buf, sMPI_East, sMPI_South, sMPI_West, sMPI_North);
